969eb3540c6d93f0043c88527d1c2e93558a402a
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-02-15 20:18:43 -0300

    chore: added config to routes

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +15,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar translucent style="light" backgroundColor='transparent' />
-      { fontsLoaded ? <SignUp /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
