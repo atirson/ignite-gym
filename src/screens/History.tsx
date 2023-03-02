105ba3bba8f335839fc2c64dbca9c44c60c26942
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-03-01 21:37:01 -0300

    feat: added component HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import { SectionList, Heading, VStack } from "native-base";
+
+import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack, Text } from "native-base";
 
 export const History = () => {
+  const [exercises, setExercises] = useState<any[]>([
+    {title: '28.08.2022', data: ['Costas', 'Bíceps']},
+    {title: '29.08.2022', data: ['Costas', 'Bíceps']},
+    {title: '30.08.2022', data: ['Costas', 'Bíceps']}
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => 
+          <HistoryCard />
+        }
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>{title}</Heading>
+        )}
+        px={8}
+        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Heading 
+            color="gray.100" 
+            fontSize="md"
+            textAlign="center" 
+          >
+              Nenhum exercício encontrado. {'\n'} Vamos treinar!
+          </Heading>
+        )}
+      />
     </VStack>
   )
 }
